ce908acb3c0739611317de085d99b11e79744b6a
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2024-12-25 01:22:14 +0530

    Create and run server

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app=express();
+const port=4000
+app.get('/',(req,res)=>{
+  res.send("Welcome")
+})
+app.listen(port,()=>{
+  console.log(`Server running on port:${port}`);
+})
