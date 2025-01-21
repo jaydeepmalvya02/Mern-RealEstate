e9e308eead7813e7c1d3260cf32af7f257cce1a6
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-01-21 14:30:37 +0530

    create  gt user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { deleteUser, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, updateUser, getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const userRouter=express.Router();
 // userRouter.get()
 userRouter.post('/update/:id',verifyToken,updateUser)
 userRouter.delete('/delete/:id',verifyToken,deleteUser)
+userRouter.get('/listings/:id',verifyToken,getUserListings)
 
 // userRouter.get('/test',test)
 
