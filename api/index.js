4b865a62b434b93c66b25faf5e909e42c9c18dd9
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-01-23 22:48:36 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 
 dotenv.config();
@@ -19,7 +20,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
   console.log("Connected to MongoDB");
 }).catch((err)=>{
   console.log(err);
-})
+});
+
+const __dirname=path.resolve();
+
 
 
 
@@ -44,6 +48,10 @@ app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter)
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+});
 //middleware
 
 app.use((err,req,res,next)=>{
