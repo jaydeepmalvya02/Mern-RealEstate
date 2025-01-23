7222ed3e556bfd2cd71683da0fdecae69feb620e
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-01-23 22:34:41 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='py-20 px-4 max-w-6xl mx-auto '>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About GharAngan</h1>
+      <p className='mb-4 text-slate-700 '>"Gharangan is a trusted real estate platform designed to simplify your property buying, selling, and renting experience. We connect buyers, sellers, and renters through a seamless and user-friendly interface, offering a wide range of verified residential and commercial properties. Whether you're searching for your dream home or listing your property for sale, Gharangan ensures transparency, reliability, and expert support every step of the way. Explore properties, connect with trusted agents, and make informed decisions with Gharangan – your gateway to hassle-free real estate solutions."</p>
+      <p className='mb-4 text-slate-700 '>
+      Our mission is To simplify and enhance the real estate experience by providing a transparent, reliable, and user-friendly platform that connects buyers, sellers, and renters with verified properties and expert support, ensuring hassle-free property transactions for everyone.
+      </p>
     </div>
   )
 }
